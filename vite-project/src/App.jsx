23e2814fa6134ab50b22466d5e793df40789a766
trinidad <daianatrinidadpuig@gmail.com>
2024-05-01 09:17:02 -0300

    Se creó la estructura general del proyecto con Vite

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,28 @@
+import Tasklist from './components/TaskList' 
+import TaskForm from './components/TaskForm'
+import { tasks as data } from "./data/tasks";
+import { useState,useEffect } from 'react';
+
+function App(){
+
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(()=>{
+   setTasks(data)
+  },[])
+
+  function createTask(task){
+    setTasks([...tasks,{
+      title:task.title,
+      id: tasks.length,
+      description:task.description
+    }])
+  }
+  return <div>
+    <TaskForm createTask={createTask}/>
+    <Tasklist tasks={tasks}/>
+  </div>
+}
+
+export default App;
+
---
+++ vite-project/src/data/tasks.js
@@ -0,0 +1,5 @@
+ export const tasks = [
+  { id: 0, title: "mi primer react", tarea: "mi primer tarea" },
+  { id: 1, title: "mi segunda react", tarea: "mi segunda tarea" },
+  { id: 2, title: "mi tercera react", tarea: "mi tercera tarea" },
+];
